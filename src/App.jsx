01458571be90207fccaf85f44e2a668a63a5c473
ShreetikaShrestha <shreetika592gmail.com>
2025-06-11 15:41:28 +0545

    nutrichart done

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,16 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import SmartBMI from './component/SmartBMI'
+import NutriPlanSuggest from './component/NutriPlanSuggest'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+   
        <SmartBMI />
+       <NutriPlanSuggest/>
     </>
   )
 }
